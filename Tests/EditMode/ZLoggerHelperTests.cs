39eb3ed41dc18ade740bf9adf8a1e992f3d2e383
Rekorn <urun4m0r1@gmail.com>
2022-11-29 02:15:12 +0900

    :white_check_mark: static logger 사용 테스트 케이스 수정

--- Tests/EditMode/ZLoggerHelperTests.cs
+++ Tests/EditMode/ZLoggerHelperTests.cs
@@ -2,19 +2,19 @@
 
 using Microsoft.Extensions.Logging;
 using NUnit.Framework;
-using Rekorn.Tools.ZLoggerHelper;
 using ZLogger;
+using static Rekorn.Tools.ZLoggerHelper.LogManager;
 
 namespace Rekorn.Tools.Tests.EditMode
 {
     public sealed class ZLoggerHelperTests
     {
-        private static readonly ILogger<ZLoggerHelperTests> s_logger = LogManager.GetLogger<ZLoggerHelperTests>();
+        private static readonly ILogger<ZLoggerHelperTests> s_logger = GetLogger<ZLoggerHelperTests>();
 
         [Test]
         public void Static_Logger_Will_Log()
         {
-            LogManager.Logger.ZLogDebug(nameof(Static_Logger_Will_Log));
+            Logger.ZLogDebug(nameof(Static_Logger_Will_Log));
         }
 
         [Test]
