9062aa965efb43e0d6f01cc1b245c05d596e8f62
Rekorn <urun4m0r1@gmail.com>
2022-11-28 23:39:45 +0900

    :wrench: 프로젝트 전역 설정 스크립트 추가

---
+++ Runtime/Core/Define.cs
@@ -0,0 +1,10 @@
+﻿#nullable enable
+
+namespace Rekorn.Tools
+{
+    public static class Define
+    {
+        public const string AssetMenuPath  = "RekornTools/";
+        public const int    AssetMenuOrder = 0;
+    }
+}
