47c31de0931657adfb0d0c2cd3af7f663626049c
Rekorn <urun4m0r1@gmail.com>
2022-11-30 01:29:14 +0900

    :memo: AppDataPath 관련 상세 주석 추가

--- Runtime/Unity/AppDataPath.cs
+++ Runtime/Unity/AppDataPath.cs
@@ -5,26 +5,72 @@ using UnityEngine;
 
 namespace Rekorn.Tools.Unity
 {
+    /// <summary>
+    /// Type of Unity application data path.
+    /// <seealso cref="UnityEngine.Application"/>
+    /// </summary>
     public enum AppDataPath
     {
+        /// <summary>
+        /// Empty string.
+        /// </summary>
         None,
-        PersistentDataPath,
-        StreamingAssetsPath,
-        DataPath,
-        TemporaryCachePath,
+        /// <summary>
+        /// Path to the console log file, or an empty string if the current platform does not support log files.
+        /// <br/><a href="https://docs.unity3d.com/ScriptReference/Application-consoleLogPath.html">Unity Docs</a>
+        /// <seealso cref="UnityEngine.Application.consoleLogPath"/>
+        /// <example>.</example>
+        /// </summary>
         ConsoleLogPath,
+        /// <summary>
+        /// (Read Only) Path to the game data folder on the target device.
+        /// <br/><a href="https://docs.unity3d.com/ScriptReference/Application-dataPath.html">Unity Docs</a>
+        /// <seealso cref="UnityEngine.Application.dataPath"/>
+        /// <example>./Assets</example>
+        /// </summary>
+        DataPath,
+        /// <summary>
+        /// (Read Only) Path to a persistent data directory.
+        /// <br/><a href="https://docs.unity3d.com/ScriptReference/Application-persistentDataPath.html">Unity Docs</a>
+        /// <seealso cref="UnityEngine.Application.persistentDataPath"/>
+        /// <example>%UserProfile%/AppData/LocalLow/%Company%/%Product%</example>
+        /// </summary>
+        PersistentDataPath,
+        /// <summary>
+        /// (Read Only) Path to the StreamingAssets folder.
+        /// <br/><a href="https://docs.unity3d.com/ScriptReference/Application-streamingAssetsPath.html">Unity Docs</a>
+        /// <seealso cref="UnityEngine.Application.streamingAssetsPath"/>
+        /// <example>./Assets/StreamingAssets</example>
+        /// </summary>
+        StreamingAssetsPath,
+        /// <summary>
+        /// (Read Only) Path to a temporary data / cache directory.
+        /// <br/><a href="https://docs.unity3d.com/ScriptReference/Application-temporaryCachePath.html">Unity Docs</a>
+        /// <seealso cref="UnityEngine.Application.temporaryCachePath"/>
+        /// <example>%Temp%/%Company%/%Product%</example>
+        /// </summary>
+        TemporaryCachePath,
     }
 
+    /// <summary>
+    /// Helper class for getting the path to the Unity application data.
+    /// <seealso cref="AppDataPath"/>
+    /// <seealso cref="UnityEngine.Application"/>
+    /// </summary>
     public static class AppDataPathHelper
     {
+        /// <summary>
+        /// Gets the path to the Unity application data.
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException"/>
         public static string GetAppDataPath(this AppDataPath appDataPath) => appDataPath switch
         {
             AppDataPath.None                => string.Empty,
+            AppDataPath.ConsoleLogPath      => Application.consoleLogPath,
+            AppDataPath.DataPath            => Application.dataPath,
             AppDataPath.PersistentDataPath  => Application.persistentDataPath,
             AppDataPath.StreamingAssetsPath => Application.streamingAssetsPath,
-            AppDataPath.DataPath            => Application.dataPath,
             AppDataPath.TemporaryCachePath  => Application.temporaryCachePath,
-            AppDataPath.ConsoleLogPath      => Application.consoleLogPath,
             _                               => throw new ArgumentOutOfRangeException(nameof(appDataPath), appDataPath, null!),
         } ?? string.Empty;
     }
