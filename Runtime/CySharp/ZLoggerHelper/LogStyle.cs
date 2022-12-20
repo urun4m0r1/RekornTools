7bb1b392a265d9b4972956bad41a2a77b9c2bc13
Rekorn <urun4m0r1@gmail.com>
2022-12-21 00:31:40 +0900

    :art: 로그 스타일 분리

---
+++ Runtime/CySharp/ZLoggerHelper/LogStyle.cs
@@ -0,0 +1,19 @@
+﻿#nullable enable
+using System;
+using UnityEngine;
+
+namespace Rekorn.Tools.ZLoggerHelper
+{
+    [Serializable]
+    public struct LogStyle
+    {
+        [field: SerializeField] public bool  IsBold { get; private set; }
+        [field: SerializeField] public Color Color  { get; private set; }
+
+        public LogStyle(bool isBold, Color color)
+        {
+            IsBold = isBold;
+            Color  = color;
+        }
+    }
+}
