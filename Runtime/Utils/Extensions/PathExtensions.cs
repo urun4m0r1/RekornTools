6f9bd1cf7de862a1d803d896631cfe795ca529dc
Rekorn <urun4m0r1@gmail.com>
2023-01-24 16:51:10 +0900

    :art: PathExtensions.cs 캐시 추가

--- Runtime/Utils/Extensions/PathExtensions.cs
+++ Runtime/Utils/Extensions/PathExtensions.cs
@@ -6,6 +6,26 @@ namespace Rekorn.Tools.Utils
 {
     public static class PathExtensions
     {
+        private static class Cache
+        {
+            public static readonly char AltDirectorySeparatorChar = Path.AltDirectorySeparatorChar;
+            public static readonly char DirectorySeparatorChar    = Path.DirectorySeparatorChar;
+
+            public static readonly string DirectorySeparatorString          = DirectorySeparatorChar.ToString();
+            public static readonly string RedundantDirectorySeparatorString = string.Concat(DirectorySeparatorString, DirectorySeparatorString);
+        }
+
+        /// <summary>
+        /// Prepend directory separator char to the start of the path.<br/>
+        /// Empty string will be returned if the path is null or whitespace.
+        /// </summary>
+        public static string PrependDirectorySeparator(this string? path)
+        {
+            return string.IsNullOrWhiteSpace(path!)
+                ? string.Empty
+                : string.Concat(Cache.DirectorySeparatorString, path);
+        }
+
         /// <summary>
         /// Append directory separator char to the end of the path.<br/>
         /// Empty string will be returned if the path is null or whitespace.
@@ -14,9 +34,14 @@ namespace Rekorn.Tools.Utils
         {
             return string.IsNullOrWhiteSpace(path!)
                 ? string.Empty
-                : string.Concat(path, Path.DirectorySeparatorChar.ToString());
+                : string.Concat(path, Cache.DirectorySeparatorString);
         }
 
+        /// <summary>
+        /// Replace all alt directory separators to default directory separators.<br/>
+        /// And remove all leading/trailing/redundant directory separators from the path.<br/>
+        /// An empty string will be returned if the path is null or whitespace.
+        /// </summary>
         public static string NormalizePath(this string? path)
         {
             // 1. return empty string if path is null or whitespace
@@ -24,20 +49,15 @@ namespace Rekorn.Tools.Utils
                 return string.Empty;
 
             // 2. replace all directory separator chars to the current platform directory separator char
-            var altSeparator = Path.AltDirectorySeparatorChar;
-            var separator    = Path.DirectorySeparatorChar;
-            path = path.Replace(altSeparator, separator);
+            path = path.Replace(Cache.AltDirectorySeparatorChar, Cache.DirectorySeparatorChar);
 
             // 3. remove all redundant directory separator chars
-            var separatorString    = separator.ToString();
-            var redundantSeparator = string.Concat(separatorString, separatorString);
-
-            while (path.Contains(redundantSeparator))
-                path = path.Replace(redundantSeparator, separatorString);
+            while (path.Contains(Cache.RedundantDirectorySeparatorString))
+                path = path.Replace(Cache.RedundantDirectorySeparatorString, Cache.DirectorySeparatorString);
 
             // 4. remove leading and trailing directory separator chars
-            path = path.TrimStart(separator) ?? string.Empty;
-            path = path.TrimEnd(separator) ?? string.Empty;
+            path = path.TrimStart(Cache.DirectorySeparatorChar) ?? string.Empty;
+            path = path.TrimEnd(Cache.DirectorySeparatorChar)   ?? string.Empty;
 
             return path;
         }
