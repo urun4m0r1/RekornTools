a97b0776f6e619931dfd676c8e475ab15d754be2
Rekorn <urun4m0r1@gmail.com>
2023-03-02 01:50:56 +0900

    :bulb: GenericValue 문서 경고 변경

--- Packages/com.urun4m0r1.rekorntools/Runtime/Utils/GenericValue.cs
+++ Packages/com.urun4m0r1.rekorntools/Runtime/Utils/GenericValue.cs
@@ -6,6 +6,7 @@ namespace Urun4m0r1.RekornTools.Utils
     /// Use this struct to solve generic classes static field conflict.
     /// <a href="https://docs.microsoft.com/en-us/dotnet/fundamentals/code-analysis/quality-rules/ca1000">CA1000</a>
     /// </summary>
+    // ReSharper disable once UnusedTypeParameter
     public struct GenericValue<TClass, TValue>
     {
         public TValue? Value { get; set; }
