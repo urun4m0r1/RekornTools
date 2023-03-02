00e20e7304d68002bd2f8ab08a1527e43531a586
Rekorn <urun4m0r1@gmail.com>
2023-03-02 17:14:30 +0900

    :fire: 미사용 Enumutility 메서드 제거

--- Packages/com.urun4m0r1.rekorntools/Runtime/Utils/EnumUtility.cs
+++ Packages/com.urun4m0r1.rekorntools/Runtime/Utils/EnumUtility.cs
@@ -8,11 +8,6 @@ namespace Urun4m0r1.RekornTools.Utils
 {
     public static class EnumUtility
     {
-        public static List<T> GetValuesList<T>() where T : Enum
-        {
-            return GetValues<T>().ToList();
-        }
-
         public static IEnumerable<T> GetValues<T>() where T : Enum
         {
             return Enum.GetValues(typeof(T)).Cast<T>();
