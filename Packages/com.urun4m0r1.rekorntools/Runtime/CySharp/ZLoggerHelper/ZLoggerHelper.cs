bc6c6044892eb218980ffc407b82323b20fca10a
Rekorn <urun4m0r1@gmail.com>
2023-05-02 01:33:41 +0900

    :recycle: 로거 코드 가독성 증가

--- Packages/com.urun4m0r1.rekorntools/Runtime/CySharp/ZLoggerHelper/ZLoggerHelper.cs
+++ Packages/com.urun4m0r1.rekorntools/Runtime/CySharp/ZLoggerHelper/ZLoggerHelper.cs
@@ -8,29 +8,19 @@ namespace Urun4m0r1.RekornTools.ZLoggerHelper
 {
     public static class LogManager
     {
+        public static ILogger Logger => s_globalLogger;
+
+        public static ILogger<T> GetLogger<T>() where T : class => s_loggerFactory.CreateLogger<T>();
+        public static ILogger    GetLogger(string categoryName) => s_loggerFactory.CreateLogger(categoryName);
+
+        public static bool IsDisposed => s_logScope.IsLoggerDisposed;
+
         private static readonly ZLoggerHelperPreset s_preset;
         private static readonly ILogger             s_globalLogger;
         private static readonly ILoggerFactory      s_loggerFactory;
 
         // ReSharper disable once NotAccessedField.Local
         private static readonly LogManagerScope s_logScope;
-
-        private static Action<ZLoggerOptions> ConfigureUnityLog() => static x =>
-        {
-            x.PrefixFormatter = static (writer, info) => s_preset.FormatUnityPrefix(info, writer);
-            x.SuffixFormatter = static (writer, info) => s_preset.FormatUnitySuffix(info, writer);
-        };
-
-        private static Action<ZLoggerOptions> ConfigureFileLog() => static x =>
-        {
-            x.PrefixFormatter = static (writer, info) => s_preset.FormatFilePrefix(info, writer);
-            x.SuffixFormatter = static (writer, info) => s_preset.FormatFileSuffix(info, writer);
-        };
-
-        private static readonly Action<ZLoggerOptions> s_unityLogConfigurator = ConfigureUnityLog();
-        private static readonly Action<ZLoggerOptions> s_fileLogConfigurator  = ConfigureFileLog();
-
-        public static bool IsDisposed => s_logScope.IsLoggerDisposed;
 
         /// <summary>
         /// 외부 static 클래스의 생명 주기 관리를 위한 클래스.
@@ -87,17 +77,17 @@ namespace Urun4m0r1.RekornTools.ZLoggerHelper
                 builder.SetMinimumLevel(s_preset.MinimumLevel);
 
                 if (s_preset.UseUnityLogging)
-                    builder.AddZLoggerUnityDebug(s_unityLogConfigurator);
+                    builder.AddZLoggerUnityDebug(ConfigureUnityLog());
 
                 if (s_preset.UseFileLogging)
-                    builder.AddZLoggerFile(s_preset.FileUrl, s_fileLogConfigurator);
+                    builder.AddZLoggerFile(s_preset.FileUrl, ConfigureFileLog());
 
                 if (s_preset.UseRollingFileLogging)
                     builder.AddZLoggerRollingFile(
                         fileNameSelector: static (dt, i) => s_preset.GetRollingFileUrl(dt, i)
                       , timestampPattern: static t => t.ToLocalTime().Date
                       , rollSizeKB: s_preset.RollingFileSizeKB
-                      , configure: s_fileLogConfigurator
+                      , configure: ConfigureFileLog()
                     );
             })!;
 
@@ -108,11 +98,21 @@ namespace Urun4m0r1.RekornTools.ZLoggerHelper
             Log("LogManager initialized.");
         }
 
-        public static ILogger Logger => s_globalLogger;
+        private static Action<ZLoggerOptions> ConfigureUnityLog() => static x =>
+        {
+            x.PrefixFormatter = static (writer, info) => s_preset.FormatUnityPrefix(info, writer);
+            x.SuffixFormatter = static (writer, info) => s_preset.FormatUnitySuffix(info, writer);
+        };
 
-        public static ILogger<T> GetLogger<T>() where T : class => s_loggerFactory.CreateLogger<T>();
-        public static ILogger    GetLogger(string categoryName) => s_loggerFactory.CreateLogger(categoryName);
+        private static Action<ZLoggerOptions> ConfigureFileLog() => static x =>
+        {
+            x.PrefixFormatter = static (writer, info) => s_preset.FormatFilePrefix(info, writer);
+            x.SuffixFormatter = static (writer, info) => s_preset.FormatFileSuffix(info, writer);
+        };
 
-        private static void Log(string message) => UnityEngine.Debug.Log($"<color=cyan><b>[Debug]</b></color> {message}");
+        private static void Log(string message)
+        {
+            UnityEngine.Debug.Log($"<color=cyan><b>[Debug]</b></color> {message}");
+        }
     }
 }
